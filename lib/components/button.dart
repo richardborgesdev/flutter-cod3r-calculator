342055aa0247464bfa99e9abc618eb18a7a6c67e
Richard Borges <richard.borges.cc@gmail.com>
2023-07-12 10:00:45 -0300

    chore: add keyboard buttons

--- lib/components/button.dart
+++ lib/components/button.dart
@@ -2,16 +2,21 @@ import 'package:flutter/material.dart';
 
 class Button extends StatelessWidget {
   final String text;
+  final bool big;
 
   const Button({
     required this.text,
+    this.big = false,
   });
 
   @override
   Widget build(BuildContext context) {
-    return TextButton(
-      onPressed: () {},
-      child: Text(text),
+    return Expanded(
+      flex: big ? 2 : 1,
+      child: TextButton(
+        onPressed: () {},
+        child: Text(text),
+      ),
     );
   }
 }
