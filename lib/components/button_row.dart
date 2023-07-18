86a7a1a838a131ca5c422c5cacbc4685218f9cba
Richard Borges <richard.borges.cc@gmail.com>
2023-07-18 14:29:28 -0300

    chore: keyboard layout spaces

--- lib/components/button_row.dart
+++ lib/components/button_row.dart
@@ -4,9 +4,7 @@ import 'button.dart';
 class ButtonRow extends StatelessWidget {
   final List<Button> buttons;
 
-  ButtonRow(
-    this.buttons,
-  );
+  ButtonRow(this.buttons);
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +12,19 @@ class ButtonRow extends StatelessWidget {
       flex: 1,
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: buttons,
+        children: buttons.fold(
+          <Widget>[],
+          (previousValue, element) {
+            previousValue.isEmpty
+                ? previousValue.add(element)
+                : previousValue.addAll([
+                    SizedBox(width: 1),
+                    element,
+                  ]);
+
+            return previousValue;
+          },
+        ),
       ),
     );
   }
