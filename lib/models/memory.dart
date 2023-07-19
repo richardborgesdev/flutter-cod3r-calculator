aae582908e13a38c32dc2840d598a8443ea387e2
Richard Borges <richard.borges.cc@gmail.com>
2023-07-19 14:49:07 -0300

    chore: set device orientation to up, add set operation and digit

--- lib/models/memory.dart
+++ lib/models/memory.dart
@@ -1,17 +1,38 @@
 class Memory {
   String _value = '0';
+  final _buffer = [0.0, 0.0];
+  int _bufferIndex = 0;
+  static const operations = [
+    '%',
+    '/',
+    'x',
+    '-',
+    '+',
+    '=',
+  ];
+  String operation = '';
 
   String get value => _value;
 
   void applyCommand(String command) {
     if (command == 'AC') {
       _allClear();
+    } else if (operations.contains(command)) {
+      _setOperation(command);
     } else {
-      _value += command;
+      _addDigit(command);
     }
   }
 
   void _allClear() {
     _value = '0';
+  }
+
+  _setOperation(String newOperation) {
+    operation = newOperation;
+  }
+
+  _addDigit(String digit) {
+    _value += digit;
   }
 }
--- lib/screens/calculator.dart
+++ lib/screens/calculator.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_cod3r_calculator/models/memory.dart';
 import '../components/display.dart';
 import '../components/keyboard.dart';
@@ -21,6 +22,10 @@ class _CalculatorState extends State<Calculator> {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
+
     return MaterialApp(
       home: Column(
         children: [
